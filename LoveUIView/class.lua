e66c5867342436d2b7eed84fc96e7a323efbc752
LexLoki <pietroribeiropepe@hotmail.com>
2016-05-14 23:44:23 -0300

    update class

--- LoveUIView/class.lua
+++ LoveUIView/class.lua
@@ -1,4 +1,15 @@
-local class = {}
+--  The MIT License (MIT)
+--  Copyright © 2016 Pietro Ribeiro Pepe.
+
+--  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+--  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Script to simulate class and inheritance in Lua
+-- Version 2.1 - 2016/05/14
+
+local class
+
 function class.new(name)
   local new_class = {}
   function new_class.newObject(...)
@@ -59,7 +70,7 @@ function class.new(name)
 end
 
 function class.extends(baseClass,name)
-  local new_class = class.new(name)
+  local new_class = class_new(name)
   if baseClass~=nil then 
     setmetatable(new_class,{__index=baseClass})
     function new_class:superClass()
@@ -68,4 +79,5 @@ function class.extends(baseClass,name)
   end
   return new_class
 end
+
 return class
